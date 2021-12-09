b0b8f7e506b9f2a8c1df25d34a26d85927e4b2ee
Xiaolin Zhou <xiaolin.zhou@mail.utoronto.ca>
2021-12-08 20:16:40 -0500

    add shiny tab panel

--- inst/shiny-scripts/app.R
+++ inst/shiny-scripts/app.R
@@ -1,74 +1,83 @@
 library(shiny)
+library(shinythemes)
 
-ui <- fluidPage(
+ui <- fluidPage(theme = shinytheme("cerulean"),
   # App title
-  headerPanel("MDRClassifier: A tool for analyzing and classifying multidrug
+  titlePanel("MDRClassifier: A tool for analyzing and classifying multidrug
               resistance (MDR) of bactria"),
+    navbarPage("Let's get started",
+      tabPanel(icon("home"),
 
-  # Sidebar panel for inputs
-  sidebarPanel(
+        # Sidebar panel for inputs
+        sidebarPanel(
 
-    selectInput(inputId="sampleType",
-                label="Sample Type",
-                choices=c("Single Isolate",
-                          "Whole Isolates",
-                          "Single Isolate from RSI",
-                          "Whole Isolates from RSI",
-                          "PCA Analysis")),
+          selectInput(inputId="sampleType",
+                      label="Sample Type",
+                      choices=c("Single Isolate",
+                                "Whole Isolates",
+                                "Single Isolate from RSI",
+                                "Whole Isolates from RSI",
+                                "PCA Analysis")),
 
-    # Single isolate graph
-    conditionalPanel(condition="input.sampleType == 'Single Isolate'",
-                     textInput(inputId="sampleID",
-                                 label="Sample ID",
-                                 value="GFBCEDDN_01248"),
-                     textInput(inputId="antibioticColName",
-                               label="Column name of antimicrobial agents",
-                               value="Antimicrobial.Agent")),
-    # Whole isolates graph
-    conditionalPanel(condition="input.sampleType == 'Whole Isolates'",
-                     textInput(inputId="antibioticColName",
-                               label="Column name of antimicrobial agents",
-                               value="Antimicrobial.Agent")),
+          # Single isolate graph
+          conditionalPanel(condition="input.sampleType == 'Single Isolate'",
+                           textInput(inputId="sampleID",
+                                       label="Sample ID",
+                                       value="GFBCEDDN_01248"),
+                           textInput(inputId="antibioticColName",
+                                     label="Column name of antimicrobial agents",
+                                     value="Antimicrobial.Agent")),
+          # Whole isolates graph
+          conditionalPanel(condition="input.sampleType == 'Whole Isolates'",
+                           textInput(inputId="antibioticColName",
+                                     label="Column name of antimicrobial agents",
+                                     value="Antimicrobial.Agent")),
 
-    # Single isolate from RSI
-    conditionalPanel(condition="input.sampleType == 'Single Isolate from RSI'",
-                     textInput(inputId="sampleRSI",
-                               label="Sample ID",
-                               value="PA1387"),
-                     selectInput(inputId="criteria",
-                                 label="Choose criteria for MDR classification",
-                                 choices=c("ECDC Criteria", "Re-defined Criteria for Incomplete Tests")),
-                     textInput(inputId="totalCat",
-                               label="Total number of antimicrobial categories for this species",
-                               value=8)),
+          # Single isolate from RSI
+          conditionalPanel(condition="input.sampleType == 'Single Isolate from RSI'",
+                           textInput(inputId="sampleRSI",
+                                     label="Sample ID",
+                                     value="PA1387"),
+                           selectInput(inputId="criteria",
+                                       label="Choose criteria for MDR classification",
+                                       choices=c("ECDC Criteria", "Re-defined Criteria for Incomplete Tests")),
+                           textInput(inputId="totalCat",
+                                     label="Total number of antimicrobial categories for this species",
+                                     value=8)),
 
-    # Whole isolates from RSI
-    conditionalPanel(condition="input.sampleType == 'Whole Isolates from RSI'",
-                     selectInput(inputId="criteria",
-                                 label="Choose criteria for MDR classification",
-                                 choices=c("ECDC Criteria", "Re-defined Criteria for Incomplete Tests")),
-                     textInput(inputId="totalCat",
-                               label="Total number of antimicrobial categories for this species",
-                               value=8)),
-    # PCA Analysis
-    conditionalPanel(condition="input.sampleType == 'PCA Analysis'",
-                     selectInput(inputId="plotType",
-                                 label="Choose the type for PCA plot",
-                                 choices=c("individual", "variables", "predict")),
-                     textInput(inputId="newDataColName",
-                               label="Enter the column names of new data for prediction",
-                               value="Specimen number,Age,Isolate number"),
-                     textInput(inputId="newData",
-                               label="Enter new data for prediction",
-                               value="1367,30,1367"),
-                     ),
-    ),
+          # Whole isolates from RSI
+          conditionalPanel(condition="input.sampleType == 'Whole Isolates from RSI'",
+                           selectInput(inputId="criteria",
+                                       label="Choose criteria for MDR classification",
+                                       choices=c("ECDC Criteria", "Re-defined Criteria for Incomplete Tests")),
+                           textInput(inputId="totalCat",
+                                     label="Total number of antimicrobial categories for this species",
+                                     value=8)),
+          # PCA Analysis
+          conditionalPanel(condition="input.sampleType == 'PCA Analysis'",
+                           selectInput(inputId="plotType",
+                                       label="Choose the type for PCA plot",
+                                       choices=c("individual", "variables", "predict")),
+                           textInput(inputId="newDataColName",
+                                     label="Enter the column names of new data for prediction",
+                                     value="Specimen number,Age,Isolate number"),
+                           textInput(inputId="newData",
+                                     label="Enter new data for prediction",
+                                     value="1367,30,1367"),
+                           ),
+          ),
 
 
-  # Main panel for displaying outputs
-  mainPanel(
-    plotOutput('plot')
-  )
+        # Main panel for displaying outputs
+        mainPanel(
+          plotOutput('plot')
+        )
+      ),
+    tabPanel("Help",
+
+
+    )
+    )
 )
 
 server <- function(input, output) {
@@ -87,30 +96,30 @@ server <- function(input, output) {
   output$plot <- renderPlot({
     if(input$sampleType == 'Single Isolate') {
       category <- MDRClassifier::classifyMDR(MDRdata(),input$sampleID,input$antibioticColName)
-      df <- classifyAllMDR(MDRdata(),input$antibioticColName)
+      df <- MDRClassifier::classifyAllMDR(MDRdata(),input$antibioticColName)
       p <- MDRClassifier::MDRPlot(df)
       mtext(paste(input$sampleID,category, sep=' '), side = 3, line = 0, outer = FALSE)
      }
     else if(input$sampleType == 'Whole Isolates') {
-      df <- classifyAllMDR(MDRdata(),input$antibioticColName)
+      df <- MDRClassifier::classifyAllMDR(MDRdata(),input$antibioticColName)
       p <- MDRClassifier::MDRPlot(df)
       }
     else if(input$sampleType == 'Single Isolate from RSI') {
-      category <- classifyMDRfromRSI(RSIdata(), input$sampleRSI, input$totalCat)
-      df <- classifyAllMDRfromRSI(RSIdata(),input$totalCat)
+      category <- MDRClassifier::classifyMDRfromRSI(RSIdata(), input$sampleRSI, input$totalCat)
+      df <- MDRClassifier::classifyAllMDRfromRSI(RSIdata(),input$totalCat)
       p <- MDRClassifier::MDRPlot(df)
       mtext(paste(input$sampleRSI, category, sep=" "), side = 3, line = 0, outer = FALSE)
     }
     else if(input$sampleType == 'Whole Isolates from RSI'){
-      df <- classifyAllMDRfromRSI(RSIdata(),input$totalCat)
+      df <- MDRClassifier::classifyAllMDRfromRSI(RSIdata(),input$totalCat)
       p <- MDRClassifier::MDRPlot(df)
     }
     else if(input$sampleType == 'PCA Analysis'){
       if(input$plotType == 'individual'){
-        p <- plotPCA(pcadata(), new_data=NULL,'individual')
+        p <- MDRClassifier::plotPCA(pcadata(), new_data=NULL,'individual')
       }
       else if(input$plotType == 'variables'){
-        p <- plotPCA(pcadata(), new_data=NULL,'variables')
+        p <- MDRClassifier::plotPCA(pcadata(), new_data=NULL,'variables')
       }
       else if(input$plotType == 'predict'){
         new_data_col <- strsplit(input$newDataColName, split=",")[[1]]
@@ -123,7 +132,7 @@ server <- function(input, output) {
          }
         df <- t(as.matrix(data_lst))
         colnames(df) <- col_lst
-        p <- plotPCA(pcadata(), df, 'predict')
+        p <- MDRClassifier::plotPCA(pcadata(), df, 'predict')
       }
     }
     print(p)
